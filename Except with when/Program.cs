5adc3138a873cf6808db056c4e218306a4766fed
Tetyana-K <tanya.kyryk@gmail.com>
2026-01-29 16:18:27 +0200

    add exceptions

---
+++ Except with when/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            DoWork();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Main зловив виняток: {ex.GetType().Name}");
+            Console.WriteLine($"Message : {ex.Message}");
+        }
+    }
+
+    static void DoWork()
+    {
+        try
+        {
+            Process();
+        }
+        catch (Exception ex) when (Log(ex))
+        {
+            throw;
+        }
+    }
+
+    static void Process()
+    {
+        int x = 0;
+        int y = 10 / x; // DivideByZeroException
+    }
+
+    static bool Log(Exception ex)
+    {
+        Console.Write(">>> Логування: ");
+        Console.WriteLine(ex.GetType().Name);
+        return false; // це важливо, щоб блок catch не виконувався
+    }
+}
---
+++ Exception intro/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine($"Input value 1:");
+double value1 = double.Parse(Console.ReadLine()!); // ! null-forgiving operator, щоб уникнути попередження компілятора
+
+Console.WriteLine($"Input value 2:");
+double value2 = double.Parse(Console.ReadLine()!);
+
+try
+{
+    double result = Div(value1, value2);
+    Console.WriteLine($"Result: {result}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Message error : {ex.Message}");
+    Console.WriteLine($"Exception type : {ex.GetType().Name}");
+    Console.WriteLine($"Target site : {ex.TargetSite}");
+    Console.WriteLine($"Data : {ex.Data}");
+    Console.WriteLine($"Stack trace : {ex.StackTrace}");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine($"Message error : {ex.Message}");
+    foreach (var key in ex.Data.Keys)
+    {
+        Console.WriteLine($"Additional data: {key} : {ex.Data[key]}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Some other exception: {ex.Message}");
+}
+double Div(double first, double second)
+{
+    if (second == 0)
+    {
+        throw new DivideByZeroException("Denominator cannot be zero.");
+    }
+    if (second > 1000)//int.MaxValue)
+    {
+        var error = new OverflowException("Denominator is too large.");
+        error.Data.Add("DenominatorValue", second);
+        error.Data.Add("Timestamp", DateTime.Now);   
+        throw error;
+    }
+    return first / second;
+}
+
