e422df5f171242d23ba3ea3d849ccdd4c35f8624
Tetyana-K <tanya.kyryk@gmail.com>
2026-02-19 20:25:39 +0200

    add IComparable example

---
+++ Implement std  IComparable/Device.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implement_std__IComparable
+{
+    public class Device : IComparable, IComparable<Device>
+    {
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public Device(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(Device? other)
+        {
+            //Console.WriteLine("\t\tCompareTo(Device) works");
+            if (other == null)
+                return 1;
+            
+            //return Name.CompareTo(other.Name); //порівнюємо за назвою
+            return Price.CompareTo(other.Price); //порівнюємо за ціною
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}  {Price}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Console.WriteLine("\t\tCompareTo(object) works");
+            if (obj == null)
+                return 1;
+            if (obj is Device otherDevice)
+                return CompareTo(otherDevice);
+            throw new ArgumentException("Object is not a Device");
+        }
+    }
+}
+// Створити клас Student (name, average score), який реалізує інтерфейс IComparable<Student>.
+// Реалізувати метод CompareTo(Student other) так,
+// щоб порівнювати студентів за їхнім середнім балом.
+// Створити кілька об'єктів класу Student як масив або як список та відсортувати їх за допомогою методу Array.Sort() або List<T>.Sort().
---
+++ Implement std  IComparable/Program.cs
@@ -0,0 +1,49 @@
+﻿using Implement_std__IComparable;
+using System.Collections;
+
+int value = 5;
+int value2 = 10;
+Console.WriteLine($"{value} compareTo {value2} = {value.CompareTo(value2)}"); //  -1
+
+string str1 = "Hello world!";
+string str2 = "Hello student!";
+Console.WriteLine($"'{str1}' compareTo '{str2}' = {str1.CompareTo(str2)}\n"); // 1
+
+
+// List<Device> - це колекція, яка може зберігати об'єкти типу Device.
+// Вона надає методи для додавання (Add), видалення (Remove) та сортування (Sort) елементів.
+List<Device> devices = new List<Device>
+{
+    new Device("Tablet", 800),
+    new Device("Laptop", 1500),
+    new Device("Phone", 799),
+    new Device("Monitor", 1200),
+};
+
+devices.Add(new Device("SmartWatch", 400)); // Додаємо новий пристрій до списку за допомогою методу Add
+
+Console.WriteLine("Before sorting:");
+foreach (var device in devices)
+    Console.WriteLine(device);
+
+devices.Sort();  // метод Sort () використовує для порівняння метод CompareTo з інтерфейсу IComparable
+
+Console.WriteLine("\nAfter sorting:");
+foreach (var device in devices)
+    Console.WriteLine(device);
+
+// ArrayList - це колекція, яка може зберігати об'єкти будь-якого типу (як object),
+
+ArrayList arrayList = new ArrayList
+{
+    new Device("IPhone", 999),
+    new Device("Xiaomi", 755),
+    new Device("Xbox", 1299),
+};
+arrayList.Add(new Device("PlayStation", 1099)); // Додаємо новий пристрій до ArrayList за допомогою методу Add
+arrayList.Sort(); // метод Sort () використовує для порівняння метод CompareTo з інтерфейсу IComparable
+Console.WriteLine($"\nAfter sorting ArrayList:");
+foreach (var d in arrayList)
+{
+    Console.WriteLine(d);
+}
